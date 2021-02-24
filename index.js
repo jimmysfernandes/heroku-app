2300a532b74bbcb96c1d6f8263bd102810da91d5
Jimmy Fernandes <jimmysfernandes@gmail.com>
2021-02-24 20:40:04 -0300

    Adjusted app port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const httpStatus = require('http-status');
 
-const port = process.env.SERVER_PORT || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(morgan('combined'));
