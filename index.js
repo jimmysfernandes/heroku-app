9f0c2557e89e5a09ecf32482fcf90d54b493f312
Jimmy Fernandes <jimmysfernandes@gmail.com>
2021-02-24 20:23:36 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const httpStatus = require('http-status');
+
+const port = process.env.SERVER_PORT || 5000;
+const app = express();
+
+app.use(morgan('combined'));
+
+app.get('/healt', (req, res) => {
+    return res.status(httpStatus.OK).json({ status: true, message: 'Here everiting OK' });
+});
+
+app.use('*', (req, res) => {
+    return res.status(httpStatus.NOT_FOUND).json({ status: false, message: `Resource ${req.path} not exists` });
+});
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
+
